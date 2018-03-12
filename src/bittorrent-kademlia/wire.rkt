6202a3811f1ac8e93853ac9fdd58d1309bc69aa4
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-03-12 18:40:41 +0000

    Tighten wire.rkt

--- src/bittorrent-kademlia/wire.rkt
+++ src/bittorrent-kademlia/wire.rkt
@@ -21,46 +21,21 @@
   (bencode->bytes (list packet)))
 
 (define (decode-packet buf [source "unknown source"])
-  (match (with-handlers [(exn? (lambda (e) #f))]
-           (bytes->bencode buf))
-    [(cons v '()) v]
-    [(cons v _)
-     (log-dht/wire-warning "DHT packet from ~a contained multiple values" source)
-     v]
-    ['()
-     (log-dht/wire-warning "DHT packet from ~a contained no values" source)
-     #f]
-    [#f
-     (log-dht/wire-warning "DHT packet from ~a contained corrupt bencoding" source)
-     #f]))
+  (define v (match (with-handlers [(exn? (lambda (e) #f))] (bytes->bencode buf))
+              [(cons v '()) v]
+              [_ #f]))
+  (when (not v)
+    (log-dht/wire-warning "Packet from ~a corrupt or invalid" source)
+    (log-dht/wire-debug "Packet from ~a corrupt or invalid: ~a" source buf))
+  v)
 
 ;;---------------------------------------------------------------------------
 ;; KRPC
 
-(define (make-packet id type . kvs)
-  (apply hash
-         #"t" id
-         #"y" (match type
-                ['request #"q"]
-                ['response #"r"]
-                ['error #"e"])
-         kvs))
-
-(define (make-query id method args)
-  (make-packet id
-               'request
-               #"q" method
-               #"a" args))
-
-(define (make-response id results)
-  (make-packet id
-               'response
-               #"r" results))
-
-(define (make-error id code message)
-  (make-packet id
-               'error
-               #"e" (list code message)))
+(define (make-packet id type . kvs) (apply hash #"t" id #"y" type kvs))
+(define (make-query id method args)  (make-packet id #"q" #"q" method #"a" args))
+(define (make-response id results)   (make-packet id #"r" #"r" results))
+(define (make-error id code message) (make-packet id #"e" #"e" (list code message)))
 
 (define (synthesize-krpc-packet id type body)
   (match type
@@ -78,7 +53,6 @@
           type
           (match type
             [#f #f]
-            ['request (list (hash-ref packet #"q" #f)
-                            (hash-ref packet #"a" hash))]
+            ['request (list (hash-ref packet #"q" #f) (hash-ref packet #"a" hash))]
             ['response (hash-ref packet #"r" hash)]
             ['error (hash-ref packet #"e" (lambda () (list #f #f)))])))
